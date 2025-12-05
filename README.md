cf2395872cba1e05f547fcca63400dd557eb34f8
QuestionBlock22 <246169733+QuestionBlock22@users.noreply.github.com>
2025-12-04 18:04:11 -0600

    Created files and double checked legitimacy.

---
+++ README.md
@@ -0,0 +1,26 @@
+## Special Item Sound Restoration
+
+This is a gecko code that makes the game play a different sound effect (Mario Kart DS' special item sound) if the item's ID is out of a certain range.
+
+Mario Kart Wii was the first game in the series to excise the special item sound effect after being used for two games. This mod restores the sound effect using the sound ripped and adjusted from Mario Kart 7 and applies it to all new items (except Thundercloud) and the returning items from Mario Kart DS. The following items are affected by this mod.
+
+* 	Lightning/Shock
+* 	Star
+* 	Golden/Dash Mushroom
+* 	Mega Mushroom
+* 	Blooper
+* 	POW Block
+* 	Bullet Bill
+* 	Triple Sets (except Triple Mushrooms)
+
+If you want to change the sound or fix bugs, all expanded resources are in the following locations in "revo_kart."
+
+	Bank Index: 738 "GRP/RACE"
+	Sequence Index: 739 "GRP/RACE"
+	Sound ID: 1EA in "EXP/SE/RC/ITEM/DECIDE/SPECIAL"
+	Bank: "RACE/UI/SP"
+	Group: "RACE"
+
+
+### Credits
+_Ro - "Play different item receive sound when receiving Shock, Star & Bullet" cheat code used as a base.
---
+++ src/PlaySpecialItemSound.s
@@ -0,0 +1,48 @@
+# Play Different Item Receive Sound When Receiving Special Items [Ro, QB22]
+
+# Modified to play the sound for more than just the Shock, Star, or Bullet Bill. Essentially restoring the special item sound from Mario Kart DS and Mario Kart Double Dash!!.
+
+# Write to:
+# PAL   : 8079814C
+# NTSC-U: 8078F140
+# NTSC-J: 807977B8
+# NTSC-K: 8078650C
+
+.set region, '' # Fill with P, E, J, or K in the quotes to assemble for a particular region.
+
+.if (region == 'P' || region == 'p') # PAL/Europe
+    .set raceStatePtr, 0x809c28d8
+.elseif (region == 'E' || region == 'e') # NTSC-U/North America
+    .set raceStatePtr, 0x809c7098
+.elseif (region == 'J' || region == 'j') # NTSC-J/Japan
+    .set raceStatePtr, 0x809c3878
+.elseif (region == 'K' || region == 'k') # NTSC-K/Korea
+    .set raceStatePtr, 0x809b4298
+.else
+    .err
+.endif
+
+# Play original item receive sound
+li r4, 0xE3
+
+# Check if in any state over than the racing state, end the code if true. (Replay Fix)
+lis r11, raceStatePtr@h
+lwz r11, -raceStatePtr@l (r11)
+lwz r0, 0x0b74 (r11)
+cmpwi r0, 0
+bne end
+
+# Subtract the item ID (register 28) value by 18 and store the contents to register 0, then check if register 0 is less than or equal to 10 (18 - 8 = Shock ID (10), 18 - 9 = Star ID (9)). Play special item sound if true.
+subi r0, r28, 18
+cmplwi r0, 10
+ble playSpecialItemSound
+
+# Check if the contents of register 28 is less than or equal to Blue Shell's item ID (0x7), then end the code if true.
+cmplwi r28, 0x7
+ble end
+
+# Play special item sound (Index 1EA in modified revo_kart.brsar)
+playSpecialItemSound:
+li r4, 0x1EA
+
+end:                             # End of function.
